ed1e30cb1bee109960ee577845becb0d01c36364
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 05:19:16 +0000

    Adding tarfile member sanitization to extractall()

--- src/nn_compress.py
+++ src/nn_compress.py
@@ -224,7 +224,26 @@ elif args.mode == 'd':
     subprocess.run([BSC_PATH,'d',args.infile,tar_archive_name])
     # untar
     with tarfile.open(tar_archive_name, "r:") as tar_handle:
-        tar_handle.extractall(tmpdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_handle, tmpdir)
     tmpfile_params = tmpdir+'params'
     tmpfile_bin_idx = tmpdir+'bin_idx'
     tmpfile_float = tmpdir+'float'
